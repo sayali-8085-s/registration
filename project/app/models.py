c1030203c2e11dd355ed4a0d8734dc09876e3cbd
sayali singh <sayalibaghel6@gmail.com>
2025-07-22 20:36:40 +0530

    n

--- project/app/models.py
+++ project/app/models.py
@@ -5,3 +5,5 @@ class student(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     document = models.FileField(upload_to='file/')
+    passw= models.CharField(max_length=50)
+    c_passw =models.CharField(max_length=50)
--- project/app/views.py
+++ project/app/views.py
@@ -8,17 +8,41 @@ def landing(req):
 
 
 def register(req):
-    return render(req,'landing.html',{'register':'register'})
-
-
-def registerdata(req):
-    print('hello')
     if req.method =='POST':
-        a=req.POST.get('name')
-        b=req.POST.get('email')
-        c=req.FILES.get('document')
-        print(a,b,c,sep=',')
+        n=req.POST.get('name')
+        e=req.POST.get('email')
+        d=req.FILES.get('document')
+        p=req.POST.get('passw')
+        cp=req.POST.get('c_passw')
 
-        student.objects.create(name=a,email=b,document=c)
-        msg ="registration data saved"
-        return render(req,'landing.html',{'msg':msg})
+        
+        data=student.objects.filter(email=e)
+        if data:
+            msg="email already exist"
+        
+            return render(req, 'landing.html', {'msg': msg})
+        else:
+            if p ==cp:
+                student.objects.create(name=n,email=e,document=d,passw=p,c_passw=cp)
+                msg ="reg sucess"
+                return render(req, 'landing.html', {'msg': msg})
+        
+
+            else:
+                    msg ="pass and cpass not match"
+                    return render(req, 'landing.html',{'msg':msg,'name':n,'email':e,'document':d})
+    else:   
+        return render(req,'landing.html',{'register':'register'})
+           
+      
+# def registerdata(req):
+#     print('hello')
+#     if req.method =='POST':
+#         a=req.POST.get('name')
+#         b=req.POST.get('email')
+#         c=req.FILES.get('document')
+#         print(a,b,c,sep=',')
+
+#         student.objects.create(name=a,email=b,document=c)
+#         msg ="registration data saved"
+#         return render(req,'landing.html',{'msg':msg})
